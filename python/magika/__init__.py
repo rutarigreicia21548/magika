8506abd3dc453c5774c79d2733066a34ceddb9e5
Rutari Greicia <rutarigreicia21548@users.noreply.github.com>
2026-04-21 17:45:55 +0800

    feat: add Python magika package __init__ with public API exports

---
+++ python/magika/__init__.py
@@ -0,0 +1,54 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Magika: AI-powered file type detection.
+
+Magika is a novel AI-powered file type detection tool that relies on the
+latest generation of deep learning to provide accurate detection even with
+very few bytes.
+
+Basic usage:
+    >>> from magika import Magika
+    >>> m = Magika()
+    >>> result = m.identify_bytes(b"# Hello World")
+    >>> print(result.output.label)
+    markdown
+"""
+
+from magika.magika import Magika
+from magika.types import (
+    MagikaResult,
+    MagikaOutputBody,
+    MagikaPrediction,
+    ModelFeatures,
+    ModelOutput,
+    ModelOutputFields,
+    PredictionMode,
+)
+
+__version__ = "0.6.0"
+__author__ = "Google LLC"
+__license__ = "Apache-2.0"
+
+__all__ = [
+    "Magika",
+    "MagikaResult",
+    "MagikaOutputBody",
+    "MagikaPrediction",
+    "ModelFeatures",
+    "ModelOutput",
+    "ModelOutputFields",
+    "PredictionMode",
+    "__version__",
+]
